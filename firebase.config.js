1b5ffa99f2281c5c7dc82f3b188d22b56b9ece38
Bradley Harrington <bharring@gmail.com>
2018-03-18 20:11:24 -0500

    add configs

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD67ytoFcqnlW3pGDwIjl8IrFrKUSEUVXg",
+  authDomain: "synchronlisity.firebaseapp.com",
+  databaseURL: "https://synchronlisity.firebaseio.com",
+  projectId: "synchronlisity",
+  storageBucket: "",
+  messagingSenderId: "166870372483"
+};
+
+export default firebaseConfig;
